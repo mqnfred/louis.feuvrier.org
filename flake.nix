f16c2a3968c2d94de52338a2e49617e4d29e2d04
Louis Feuvrier <louis@feuvrier.org>
2022-08-18 14:24:15 -0700

    Initial commit
    Here is what we build today:
    
     - .#resume is the one page resume as a pdf
     - .#www (default) is my personal page, including the resume

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "louis.feuvrier.org website";
+
+  inputs = rec {
+    nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
+    utils.url = "github:numtide/flake-utils";
+    utils.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs: inputs.utils.lib.eachDefaultSystem (system: let
+    pkgs = import inputs.nixpkgs { inherit system; };
+    buildInputs = [
+      pkgs.texlive.combined.scheme-full
+      pkgs.zola
+    ];
+
+    resumeFileName = "louis_feuvrier-resume.pdf";
+    resume = pkgs.stdenvNoCC.mkDerivation rec {
+      inherit buildInputs;
+      src = ./resume;
+      name = "resume";
+      buildPhase = "xelatex -output-directory=. resume.tex";
+      installPhase = ''
+        mkdir -p $out
+        cp resume.pdf $out/${resumeFileName}
+      '';
+    };
+
+    www = pkgs.stdenvNoCC.mkDerivation rec {
+      inherit buildInputs;
+      name = "www";
+      src = ./www;
+
+      phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+      buildPhase = "zola build --output-dir public";
+      installPhase = ''
+        cp -r public $out;
+        cp ${resume}/${resumeFileName} $out/${resumeFileName};
+      '';
+    };
+  in rec { # per-system output
+    devShell = pkgs.mkShell { inherit buildInputs; };
+    packages = rec {
+      inherit resume www;
+      default = www;
+    };
+  });
+}
---
+++ www/content/_index.md
@@ -0,0 +1,6 @@
++++
++++
+20 years of computers later, I am still building. From kernels to large
+distributed systems, I have seen software evolve across scales. These days, I
+especially enjoy applying computer science to the software development
+pipeline.
